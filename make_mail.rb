bd3528a7232da143f9a1d5de038a3ba38fcfd219
yhatanaka <y.hatanaka@gmail.com>
2020-04-09 14:08:52 +0900

    change form Monday to Friday and Sunday

--- make_mail.rb
+++ make_mail.rb
@@ -7,35 +7,22 @@ toAddress = 'mizuochi@sakatafm.com'
 episodeNumberString = ['', '前半', '後半']
 
 
-# 毎月1〜7日の月曜日が前半の1回目オンエア日，16〜22日の月曜日が後半の1回目オンエア日
-# 提出は前週の金曜なので，オンエア日3日前
-# 提出日の3日後以降に，月曜 && ( 日付が ( 1-7 #1) or ( 16-22 #2) )があれば，1) なら前半，2) なら後半
+# 毎月1〜7日の金曜日 or 日曜日が前半の1回目オンエア日，16〜22日の金曜日 or 日曜日が後半の1回目オンエア日
+# 提出は月or火曜なので，オンエア日3 or4日前
+# 提出日の3日後以降に，(金曜 or 日曜) && ( 日付が ( 1-7 #1) or ( 16-22 #2) )があれば，1) なら前半，2) なら後半
 
 # 3日後から始めて1週間（10日後まで）
 todaysDate = Date.today
 # 3日後以降，3週間分（3〜23）
 nextOnAirDay = (3..23).each do |i|
-	serchedDay = todaysDate + i
-#	if ( \
-#		serchedDay.wday == 1 \
-#	) && ( \
-#		( \
-#			serchedDay.day <= 7 \
-#		) || ( \
-#			( \
-#				serchedDay.day > 15 \
-#			) && ( \
-#				serchedDay.day <= 22 \
-#			) \
-#		) \
-#	) then
-	if serchedDay.wday == 1 then
-		if serchedDay.day <= 7 then
+	searchedDay = todaysDate + i
+	if (searchedDay.wday == 0 || searchedDay.wday == 5) then
+		if searchedDay.day <= 7 then
 			episodeNumber = 1
-			break [serchedDay, episodeNumber]
-		elsif serchedDay.day > 15 && serchedDay.day <= 22
+			break [searchedDay, episodeNumber]
+		elsif searchedDay.day > 15 && searchedDay.day <= 22
 			episodeNumber = 2
-			break [serchedDay, episodeNumber]
+			break [searchedDay, episodeNumber]
 		end #if
 	end #if
 end #each
@@ -44,7 +31,7 @@ episodeYear = nextOnAirDay[0].year
 episodeMonth = nextOnAirDay[0].month
 episodeNumber = nextOnAirDay[1]
 
-mailTitle = 'ノラーソ・ビヤ・マールキ 2018年' + episodeMonth.to_s + '月#' + episodeNumber.to_s
+mailTitle = 'ノラーソ・ビヤ・マールキ ' + episodeYear.to_s + '年' + episodeMonth.to_s + '月#' + episodeNumber.to_s
 
 mailBodyOrig = <<EOS
 水落さま
