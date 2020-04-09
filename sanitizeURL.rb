99678a7f20b097524ce8afd4a2c4c391bddc3c1b
yhatanaka <y.hatanaka@gmail.com>
2020-04-09 14:09:07 +0900

    mod

--- sanitizeURL.rb
+++ sanitizeURL.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/ruby
 require "pp"
+require 'uri'
 result = Array.new
 
-inputURL = File.read(ARGV.shift)
-#inputURL = ARGV.shift
+#inputURL = File.read(ARGV.shift)
+inputURL = ARGV.shift
 
 def url_http(str)
 	if (str.match(%r{.+(https?://.+)}))
@@ -17,12 +18,19 @@ def removeGetParam(str)
 	return str.gsub(/[&\?][a-z0-9]+=.*/, "")
 end
 
+def removeCommaDigit(str)
+	return str.sub(/\/?,[0-9,\-]+/,"")
+end
+
+def removeParam(str)
+	return str.sub(/html\?.+$/,"html")
+end
 
 inputURLary = inputURL.split("\n")
 
 inputURLary.each do |f|
 
-	sanitizedURL = removeGetParam(url_http(f))
+	sanitizedURL = removeParam(removeCommaDigit(removeGetParam(url_http(URI.unescape(f)))))
 	result.push(sanitizedURL)
 
 end
