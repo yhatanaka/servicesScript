f7e1913742b967af25d8430c069faee1076a8828
yhatanaka <32429411+yhatanaka@users.noreply.github.com>
2024-03-22 15:55:27 +0900

    input fix

--- cal2csv_cot.rb
+++ cal2csv_cot.rb
@@ -1,4 +1,6 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby -Ku
+#%%%{CotEditorXInput=AllText}%%%
+#%%%{CotEditorXOutput=ReplaceAllText}%%%
 
 # "カレンダー"からコピーしたイベントをペーストしたテキストファイルを，timeline2で読み込めるcsvに変換
 
@@ -10,9 +12,9 @@ headerArray = ['Start Date','End Date','Event Title']
 outputArray = Array.new
 outputCSVString = CSV::generate_line(headerArray)
 
-open(ARGV.shift,'r') do |f|
 	eventArray = Array.new(3)
-	f.each do |line|
+while $stdin.gets
+	line = $_
 		if /^予定日: (20[12][0-9])\/([01][0-9])\/([0-3][0-9]) ([0-9:]+)〜([0-9:]+)/.match(line)
 			eventArray[0] = $1 + '-' + $2 + '-' + $3 + ' ' + $4 + ':00'
 			eventArray[1] = $1 + '-' + $2 + '-' + $3 + ' ' + $5 + ':00'
@@ -31,6 +33,5 @@ open(ARGV.shift,'r') do |f|
 		else
 			eventArray[2] = line.rstrip
 		end #if
-	end # inFile.each
-end # do inFile
+end #inFile
 print outputCSVString
