3055c9ef730a80bdb320312b8700ade6589ac20c
yhatanaka <32429411+yhatanaka@users.noreply.github.com>
2024-03-22 15:49:40 +0900

    add

---
+++ cal2csv_cot.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby
+
+# "カレンダー"からコピーしたイベントをペーストしたテキストファイルを，timeline2で読み込めるcsvに変換
+
+require 'date'
+require 'pp'
+require 'csv'
+
+headerArray = ['Start Date','End Date','Event Title']
+outputArray = Array.new
+outputCSVString = CSV::generate_line(headerArray)
+
+open(ARGV.shift,'r') do |f|
+	eventArray = Array.new(3)
+	f.each do |line|
+		if /^予定日: (20[12][0-9])\/([01][0-9])\/([0-3][0-9]) ([0-9:]+)〜([0-9:]+)/.match(line)
+			eventArray[0] = $1 + '-' + $2 + '-' + $3 + ' ' + $4 + ':00'
+			eventArray[1] = $1 + '-' + $2 + '-' + $3 + ' ' + $5 + ':00'
+			outputCSVString << CSV::generate_line(eventArray)
+		elsif /^予定日: (20[12][0-9])\/([01][0-9])\/([0-3][0-9])から(20[12][0-9])\/([01][0-9])\/([0-3][0-9])/.match(line)
+#		elsif /^予定日: (20[12][0-9])\/([01][0-9])\/([0-3][0-9])〜(20[12][0-9])\/([01][0-9])\/([0-3][0-9])/.match(line)
+			eventArray[0] = $1 + '-' + $2 + '-' + $3 + ' ' + '00:00'
+			eventArray[1] = $4 + '-' + $5 + '-' + $6 + ' ' + '24:00'
+			outputCSVString << CSV::generate_line(eventArray)
+		elsif /^予定日: (20[12][0-9])\/([01][0-9])\/([0-3][0-9])/.match(line)
+			eventArray[0] = $1 + '-' + $2 + '-' + $3 + ' ' + '00:00'
+			eventArray[1] = $1 + '-' + $2 + '-' + $3 + ' ' + '24:00'
+			outputCSVString << CSV::generate_line(eventArray)
+		elsif /場所: (.+)/.match(line)
+		elsif /〒.+/.match(line)
+		else
+			eventArray[2] = line.rstrip
+		end #if
+	end # inFile.each
+end # do inFile
+print outputCSVString
