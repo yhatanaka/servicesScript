0ba83049346b3ae3fa8e79916f103d0bb85d9d60
yhatanaka <32429411+yhatanaka@users.noreply.github.com>
2025-03-22 15:49:15 +0900

    add for gem

---
+++ oga_test.rb
@@ -0,0 +1,16 @@
+#!ruby
+# -*- coding: utf-8 -*-
+#$KCODE='UTF8'
+Encoding.default_external = "UTF-8"
+
+require 'fileutils'
+require 'pp'
+
+require 'Oga'
+exit
+
+#ENV['GEM_PATH']='/Users/hatanaka/.gem/ruby/3.4.0:/Users/hatanaka/.rbenv/versions/3.4.2/lib/ruby/gems/3.4.0:/Users/hatanaka/Documents/servicesScript/HOME/.gem'
+
+pp ENV
+
+exit
