86d5b695b608348cc85f19139e64bb5a85345207
yhatanaka <32429411+yhatanaka@users.noreply.github.com>
2025-11-06 11:26:13 +0900

    Update sanitizeURL_cot.rb
    mod

--- sanitizeURL_cot.rb
+++ sanitizeURL_cot.rb
@@ -19,7 +19,6 @@ end
 
 def removeDir(str)
 	return str.sub(/\/[a-zA-Z0-9_]+\.[a-zA-Z]+\?[a-zA-Z]+\=/, "")
-
 end
 
 def removeGetParam(str)
@@ -34,16 +33,35 @@ def removeParam(str)
 	return str.sub(/html\?.+$/,"html")
 end
 
+def removeRedirect(str)
+	thisURI = URI.parse(str)
+	host = thisURI.host
+	newURL = ''
+	thisURI = URI.parse(str)
+	query = URI.decode_www_form(thisURI.query.to_s).to_h
+	query.each {|idx,value|
+		if /[\/a-zA-Z0-9\-\.]/.match(value) && /\//.match(value)
+# URLのドメイン以下の部分、'/'で始まっていたら削除
+			newURL = thisURI.scheme + '://' + thisURI.host + '/' + value.sub(/^\/?/, '')
+		end
+	}
+	return newURL
+end
 
 #inputURLary = inputURL.split("\n")
 
 #inputURLary.each do |f|
 while $stdin.gets
 	f = $_
+	sanitizedURL = f
 	sanitizedURL = url_http(URI.unescape(f))
+	sanitizedURL = removeRedirect(sanitizedURL)
 	sanitizedURL = removeDir(sanitizedURL)
-	sanitizedURL = removeParam(removeCommaDigit(removeGetParam(sanitizedURL)))
-	result.push(sanitizedURL)
+	sanitizedURL = removeGetParam(sanitizedURL)
+	sanitizedURL = removeCommaDigit(sanitizedURL)
+	sanitizedURL = removeParam(sanitizedURL)
+#	sanitizedURL = removeParam(removeCommaDigit(removeGetParam(sanitizedURL)))
+	result.push(sanitizedURL.chomp)
 
 end
 puts result.uniq.sort.join("\n")
