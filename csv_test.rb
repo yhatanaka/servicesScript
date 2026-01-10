0819b4fd4d2f6d242d94ab46efa31709d5461d46
yhatanaka <32429411+yhatanaka@users.noreply.github.com>
2026-01-10 17:41:42 +0900

    add test for composite key

--- csv_test.rb
+++ csv_test.rb
@@ -9,15 +9,16 @@ require 'csv'
 require 'pp'
 require_relative 'CsvTableUtil.rb'
 
-actualTestFlag = true
+actualTestFlag = false
 
 if actualTestFlag
 	require 'minitest/autorun' # Minitest ライブラリを読み込む
 else
 	base_dir = '/Users/hatanaka/Documents/servicesScript/csv_test'
 	inputFile1 = "#{base_dir}/1-test_1.csv"
-	inputFile2 = "#{base_dir}/1-test_2.csv" # 縦持ち
-	pp unpivotedTable2Data(origTable(inputFile2), [:番号])
+	pp :test
+	pp :test.to_s
+	pp :test.to_sym
 	exit
 
 end
@@ -37,6 +38,7 @@ EOS
 		assert_equal ["番号", "氏名", "年齢", "変なヤツ"], origHeader(inputFile1) # ヘッダ
 		assert_equal "番号,name,年齢,idiot\n" + "1,名無　権兵衛,51,TRUE\n" + "2,普通　野子,30,FALSE\n" + "3,南野　誰某,43,TRUE\n", replaceHeaders(inputFile1, [nil,'name',nil,'idiot']).to_s # ヘッダ置換
 		assert_equal "番号,氏名\n1,名無　権兵衛\n2,普通　野子\n3,南野　誰某\n", selectTableCol(origTable(inputFile1), [:番号, :氏名]).to_s # 特定のヘッダのみ
+
 		dataH = {{番号: 1} => {氏名: '名無　権兵衛', 年齢: 51, 変なヤツ: 'TRUE'}, {番号: 2} => {氏名: '普通　野子', 年齢: 30, 変なヤツ: 'FALSE'}, {番号: 3} => {氏名: '南野　誰某', 年齢: 43, 変なヤツ: 'TRUE'}} # Data
 		assert_equal dataH, pivotedTable2Data(origTable(inputFile1), [:番号]) # 横持ち -> Data
 		unpivotTblStr = <<EOS
@@ -55,6 +57,14 @@ EOS
 		inputFile2 = "#{base_dir}/1-test_2.csv" # Data -> 縦持ち
 		assert_equal dataH, unpivotedTable2Data(origTable(inputFile2), [:番号]) # 縦持ち -> Data
 		assert_equal origTblStr, data2PivotedTable(dataH, ["番号", "氏名", "年齢", "変なヤツ"].map{|n| n.to_sym}).to_csv # Data -> 横持ち
+
+		inputFileCompoKeyPivot = "#{base_dir}/2-test_n.csv" # 複合キー
+		inputFileCompoKeyUnpivot = "#{base_dir}/2-test_n+1.csv" # 複合キー
+		assert_equal replaceHeaders(inputFileCompoKeyUnpivot,[nil,nil,'otherKey','otherKeysValue']).to_s, unpivot(origTable(inputFileCompoKeyPivot), [:名前, :年月日]).to_s # 横 -> 縦
+
+		unpivotReplTbl = replaceHeaders(inputFileCompoKeyUnpivot,[nil,nil,'otherKey','otherKeysValue']) # 縦 -> 横
+		assert_equal origTable(inputFileCompoKeyPivot), pivot(unpivotReplTbl, [:名前, :年月日], headersAry = [:名前, :年月日, :朝食, :昼食, :おやつ, :夕食]) # 縦 -> 横
+
 	end
 	
 	# def test_diff
