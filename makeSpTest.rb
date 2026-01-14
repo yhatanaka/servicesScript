c3e9c4980f192478dcecb1dcf079736e1cfcfd1f
yhatanaka <32429411+yhatanaka@users.noreply.github.com>
2026-01-14 19:39:17 +0900

    add comment

--- makeSpTest.rb
+++ makeSpTest.rb
@@ -29,11 +29,16 @@ class MakeSpTest < Minitest::Test # Minitest::Test クラスを継承
 		pickFile = "#{base_dir}/locSp.txt"
 		pickDepthAry = [0,nil,1]
 		fromFile = "#{base_dir}/spList.csv"
+# 階層1,階層3を読み込む
 		ret01Ary = [['泥沢', 'モノサシトンボ'], ['泥沢', 'コバネイバゴ'], ['藤井公園', 'モリアオガエル'], ['藤井公園', 'モリアオガエル'], ['藤井公園', 'カナヘビ'], ['ハッチョウ', 'モリアオガエル'], ['ハッチョウ', 'ハッチョウトンボ']]
-		assert_equal ret01Ary, locSpArys(File.read(pickFile), pickDepthAry)
+# 調査結果
+		researchList = File.read(pickFile)
+		assert_equal ret01Ary, locSpArys(researchList, pickDepthAry)
+# 種名 => [調査地1, 調査地2]
 		ret2Hash = {'モノサシトンボ' => ['泥沢'], 'コバネイバゴ' => ['泥沢'], 'モリアオガエル' => ['藤井公園', 'ハッチョウ'], 'カナヘビ' => ['藤井公園'], 'ハッチョウトンボ' => ['ハッチョウ']}
 		# assert_equal ret2Hash, makeSp2LocHash(ret01Ary)
-		assert_equal ret2Hash, makeSp2LocHash(locSpArys(File.read(pickFile), pickDepthAry))
+		assert_equal ret2Hash, makeSp2LocHash(locSpArys(researchList, pickDepthAry))
+# 種名リストの重複チェック
 		spTbl = origTable(fromFile)
 		assert_equal ['ヤマキマダラヒカゲ'], checkDupSpList(spTbl)
 		assert_equal [4, 5], whereSpList(spTbl, 'ヤマキマダラヒカゲ')
